f1ebd35624e3084e770cf47fa2d3948e4f22ef54
EdwinSch <edwinschutjes@gmail.com>
2023-05-26 14:06:51 +0200

    add: input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,9 @@
+const Input = () => {
+  return (
+    <form className="form-control">
+      <input type="text" placeholder="Create new todo..." name="" id="" />
+    </form>
+  );
+};
+
+export default Input;
