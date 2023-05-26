e5f5d6199f486f9918a66b38ca0600910987899f
EdwinSch <edwinschutjes@gmail.com>
2023-05-26 16:33:52 +0200

    update: list component

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,16 +1,24 @@
 import Buttons from "./Buttons";
 
+import { AiOutlineClose } from "react-icons/ai";
+
 const List = () => {
   return (
     <section className="list-container">
       <ul className="list">
         <li className="item">
           <input type="checkbox" name="" id="" />
-          Some item
+          <div className="flex-container">
+            Some item
+            <AiOutlineClose className="delete-item" />
+          </div>
         </li>
         <li className="item">
           <input type="checkbox" name="" id="" />
-          Some item
+          <div className="flex-container">
+            Some item 2
+            <AiOutlineClose className="delete-item" />
+          </div>
         </li>
       </ul>
       <Buttons />
