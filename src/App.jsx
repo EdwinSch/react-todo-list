94ec5ee793f271d2cf0bbcdf0e4c31f2aaf50ae3
EdwinSch <edwinschutjes@gmail.com>
2023-05-26 15:51:55 +0200

    add: list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Input from "./components/Input";
+import List from "./components/List";
 
 function App() {
   return (
     <main>
       <Header />
       <Input />
+      <List />
     </main>
   );
 }
---
+++ src/components/List.jsx
@@ -0,0 +1,18 @@
+const List = () => {
+  return (
+    <section className="list-container">
+      <ul className="list">
+        <li className="item">
+          <input type="checkbox" name="" id="" />
+          Some item
+        </li>
+        <li className="item">
+          <input type="checkbox" name="" id="" />
+          Some item
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default List;
